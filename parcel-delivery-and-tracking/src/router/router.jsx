d2c6d180ff7767f1a03d14412b05d12af6ce3bf1
Saad7890-web <omysaadislam@gmail.com>
2025-12-18 22:57:13 +0600

    project setup done

---
+++ parcel-delivery-and-tracking/src/router/router.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-7xl">Hello World</div>,
+  },
+]);
